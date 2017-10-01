8823f8506c1d9f96c4b17c905165683d4476112c
Francois ANDRE <francois.andre@carnavello.fr>
2017-10-01 22:46:38 +0200

    Ajout chargement par version

--- aerisComponentLoader.js
+++ aerisComponentLoader.js
@@ -17,6 +17,8 @@ function loadJsFromGithub(path) {
 			console.log("Version: "+version)
 			if (version.toLowerCase() == "latest") {
 				console.log(getLatestUrl(account, repo))
+			} else {
+				console.log(getVersionUrl(account, repo, version))
 			}
 		}
 	}
@@ -37,6 +39,42 @@ function getLatestUrl(account, repo) {
 	    var latestRelease = json[0];
 		var sha =latestRelease.commit.sha;
 		var version = latestRelease.name;
+		var rawGitUrl = getRawgitUrl(account, repo, sha, version)
+		console.log(rawGitUrl)
+		var scriptTag = document.createElement('script');
+		scriptTag.setAttribute('src',rawGitUrl);
+		document.head.appendChild(scriptTag);
+	  } else {
+	    console.log("Problem with getLatestUrl "+url)
+
+	  }
+	};
+
+	request.onerror = function() {
+		console.log("Problem with getLatestUrl "+url)
+	};
+
+	request.send();
+}
+
+function getVersionUrl(account, repo,version) {
+	var url = "https://sedoo.aeris-data.fr/aeris-rest-services/rest/jsloading/versions?component="+repo+"&fakeparam="+Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10)
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+
+	request.onload = function() {
+	  if (request.status >= 200 && request.status < 400) {
+	    // Success!
+	    var json = JSON.parse(request.responseText);
+	    var latestRelease = null;
+	    var arrayLength = json.length;
+	    for (var i = 0; i < arrayLength; i++) {
+	        if (json[i].name == version) {
+	        	latestRelease = json[i]
+	        }
+	    }
+	    
+		var sha =latestRelease.commit.sha;
 		var rawGitUrl = getRawgitUrl(account, repo, sha, version)
 		console.log(rawGitUrl)
 		var scriptTag = document.createElement('script');
